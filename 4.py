1f9276ed03f80c354701b40982afddc3f55e1a9d
patjm1992 <patjm1992@gmail.com>
2016-10-20 16:33:34 -0400

    #3 done

--- 4.py
+++ 4.py
@@ -1,3 +1,4 @@
+import timeit
 
 def is_palidrome(n):
     n = str(n)
@@ -7,7 +8,19 @@ def is_palidrome(n):
     else:
         return False
 
-print is_palidrome(100)
-print is_palidrome(1001)
-print is_palidrome(0)
-print is_palidrome(34043)
+start_time = timeit.default_timer()
+
+res = 0
+
+for a in range(999, 0, -1):
+    for b in range(a, 0, -1):
+        prod = a * b
+        if is_palidrome(prod):
+            if prod > res:
+                res = prod
+
+elapsed = timeit.default_timer() - start_time
+
+print res
+
+print("Evaluated in %.10f seconds." % elapsed)
