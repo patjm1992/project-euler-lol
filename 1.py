e04484749a5339d0d58d8440829f3337e65e86ba
patjm1992 <patjm1992@gmail.com>
2016-10-20 13:17:03 -0400

    added timer to #1

--- 1.py
+++ 1.py
@@ -1,2 +1,9 @@
-s = sum([x for x in range(1000) if x % 3 == 0 or x % 5 == 0])
-print(s)
+import timeit
+
+start_time = timeit.default_timer()
+
+print(sum([x for x in range(1000) if x % 3 == 0 or x % 5 == 0]))
+
+elapsed = timeit.default_timer() - start_time
+
+print("Evaluated in %.10f seconds." % elapsed)
