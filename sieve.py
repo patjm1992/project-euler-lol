f9277b1a422db478bcdfbcb1fb27bd15e6625e63
patjm1992 <patjm1992@gmail.com>
2016-11-01 10:35:27 -0400

    sieve algorithm

---
+++ sieve.py
@@ -0,0 +1,25 @@
+import timeit
+import math
+
+# Sieve of Eratosthenes algorithm
+def sieve(n):
+
+    # List containing 2 and odd numbers
+    nums = [x for x in xrange(2, n) if x == 2 or x % 2 != 0]
+
+    p = 2
+
+    for p in nums:
+
+        for i in xrange(p + p, n + 1, p):
+            if i in nums:
+                nums.remove(i)
+    return nums
+
+start_time = timeit.default_timer()
+
+print sieve(100000)
+
+elapsed = timeit.default_timer() - start_time
+
+print("Evaluated in %.10f seconds." % elapsed)
